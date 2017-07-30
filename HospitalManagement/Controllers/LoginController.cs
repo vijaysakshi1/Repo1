89bfca87a14c5f45c6a67a144d9e2968605c1ebb
vijaysakshi1 <vijay.sakshi12@gmail.com>
2017-07-30 16:25:27 +0530

    initial commit

---
+++ HospitalManagement/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HospitalManagement.Models;
+
+namespace HospitalManagement.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            LoginModel obj = new LoginModel();
+            
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Index(LoginModel objlogin)
+        {
+            
+
+            return View(objlogin);
+        }
+    }
+}
